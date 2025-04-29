d00196cd3f57d3df55a23f96b72d7c770a608787
Muxomor <7baltika9@gmail.com>
2025-04-29 12:49:51 +0300

    traefik things

--- Program.cs
+++ Program.cs
@@ -3,28 +3,29 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WTExpCalc;
 using WTExpCalc.Services;
 using System.Net.Http;
-using System.Text.Json;
+using System.Text.Json; // Убедитесь, что using есть
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// читаем URL из appsettings.json (или дефолт)
-var postgrestUrl = builder.Configuration["Postgrest:Url"]
-                  ?? "http://localhost:3000";
-
-// регистрируем HttpClient c базовым адресом PostgREST
+// Регистрация HttpClient для отправки запросов на тот же origin,
+// с которого загружено приложение. Traefik будет маршрутизировать /api/*
 builder.Services.AddScoped(sp => new HttpClient
 {
-    BaseAddress = new Uri(postgrestUrl)
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
 });
 
-// наш сервис, принимает HttpClient через ctor
+// Регистрация вашего сервиса API
 builder.Services.AddScoped<IExperienceApi, ExperienceApiService>();
+
+// Настройка опций JSON сериализации (если ваш API использует snake_case)
+// Оставьте этот блок, если он нужен вашему API (PostgREST обычно использует snake_case)
 builder.Services.Configure<JsonSerializerOptions>(options =>
 {
     options.PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower;
-    options.PropertyNameCaseInsensitive = true;
+    options.PropertyNameCaseInsensitive = true; 
 });
+
 
 await builder.Build().RunAsync();
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,5 @@
 NGINX_ROOT=/usr/share/nginx/html
-PLACEHOLDER="__POSTGREST_URL__" # Плейсхолдер, который мы ищем
 
-# Ищем файл appsettings (может быть .json или .Production.json и т.д.)
-APPSETTINGS_FILE=$(find $NGINX_ROOT -name 'appsettings*.json' -print -quit)
-
-# Получаем значение переменной окружения POSTGREST_URL
-# Если переменная не задана, используем значение по умолчанию
-# ВАЖНО: Установите осмысленное значение по умолчанию или оставьте пустым, если это предпочтительнее
-POSTGREST_URL_VALUE=${POSTGREST_URL:-"http://localhost:3000"}
-
-if [ -z "$APPSETTINGS_FILE" ]; then
-  echo "[ENTRYPOINT] ПРЕДУПРЕЖДЕНИЕ: Файл appsettings*.json не найден в $NGINX_ROOT."
-elif [ -z "$POSTGREST_URL" ]; then
-  echo "[ENTRYPOINT] ПРЕДУПРЕЖДЕНИЕ: Переменная окружения POSTGREST_URL не установлена. Используется значение по умолчанию: $POSTGREST_URL_VALUE"
-  echo "[ENTRYPOINT] Замена '$PLACEHOLDER' на '$POSTGREST_URL_VALUE' в $APPSETTINGS_FILE"
-  sed -i "s|$PLACEHOLDER|${POSTGREST_URL_VALUE}|g" "$APPSETTINGS_FILE"
-else
-  echo "[ENTRYPOINT] Замена '$PLACEHOLDER' на '$POSTGREST_URL_VALUE' (из ENV POSTGREST_URL) в $APPSETTINGS_FILE"
-  # Используем '|' как разделитель в sed, чтобы избежать конфликтов с '/' в URL
-  sed -i "s|$PLACEHOLDER|${POSTGREST_URL_VALUE}|g" "$APPSETTINGS_FILE"
-fi
 
 # Запускаем Nginx в режиме foreground (чтобы контейнер не завершался)
 echo "[ENTRYPOINT] Запуск Nginx..."
